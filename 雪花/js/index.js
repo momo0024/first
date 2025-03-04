63e5ee1749bf93e949d2fc39e384f93e789d300a
keria <55522395+momo0024@users.noreply.github.com>
2025-03-04 10:57:48 +0800

    Update index.js
    修改雪花显示

--- 雪花/js/index.js
+++ 雪花/js/index.js
@@ -22,7 +22,7 @@
 			// var xuecolor=rgb($(r),$(g),$(b));
 			var xue=$('<div></div>').css({
 				'position':'absolute',
-			}).html('❉');
+			}).html('&#10052');
 			setInterval(function(){
 				var xuesize=xuemin+Math.random()*xuemax;
 				xue.clone().appendTo($('body')).css({
